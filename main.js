249c6e8d3b49f29ff4820def3ad4608f7c6e3c49
yeonding <yeonding2@naver.com>
2023-05-03 14:56:32 +0900

    [추가|0503] 기본 html 추가.

---
+++ main.js
@@ -0,0 +1,7 @@
+// 자바스크립트 코드
+// window.addEventListener('load', function() {
+//     // 이미지가 로딩된 후에 3초 뒤에 메인 페이지로 이동
+//     setTimeout(function() {
+//         window.location.href = "./main.html";
+//     }, 3000); // 3초 (3000밀리초) 후에 이동
+// });
