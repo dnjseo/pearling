cc429afeb9bc64f2c06d6bc003ce3ca5b6a106b0
one4961 <one496100@gmail.com>
2023-05-10 17:51:20 +0900

    [추가|0510] header 완성 및 프로필 화면 추가

--- js/header.js
+++ js/header.js
@@ -1,6 +1,19 @@
 fetch("header.html")
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("header").innerHTML = data;
-            })
-            .catch(error => console.error(error));
+  .then((response) => response.text())
+  .then((data) => {
+      document.getElementById("header").innerHTML = data;
+      
+      var menu = document.getElementById('menu');
+      var subMenu = document.getElementById('sub_menu');
+      var close = document.getElementById('close');
+      
+      menu.addEventListener('click', function() {
+        subMenu.style.display = 'block';
+      });
+
+      close.addEventListener('click', function(){
+        subMenu.style.display = 'none';
+      });
+  })
+  .catch((error) => console.error(error));
+
