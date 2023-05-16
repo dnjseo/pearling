58d062f534f352ff0928292ea5a3234ac2971918
one4961 <one496100@gmail.com>
2023-05-16 15:45:18 +0900

    [수정|0516] footer 수정

--- js/footer/footer_list.js
+++
@@ -1,7 +0,0 @@
-
-fetch("/html/footer/footer_list.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("footer_list").innerHTML = data;
-  })
-  .catch((error) => console.error(error));
